8f8c0535f58f3bf99d8d7a8a205a9fedd9c0bc00
JhonT01 <jftc_94@hotmail.com>
2021-11-11 00:19:53 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,22 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  generador(pronoun, adj, noun, domain);
+};
+
+let pronoun = ["the", "our", "last"];
+let adj = ["great", "big", "Of"];
+let noun = ["jogger", "racoon", "Us"];
+let domain = [".com", ".es", ".org"];
+
+const generador = (pronoun, adj, noun, domain) => {
+  for (const parte1 of pronoun) {
+    for (const parte2 of adj) {
+      for (const parte3 of noun) {
+        for (const parte4 of domain) {
+          console.log(parte1 + parte2 + parte3 + parte4);
+        }
+      }
+    }
+  }
 };
